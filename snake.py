c165b33189d24607f8ac12e604ea0ec62b6bcc4c
L3M0NFLUX <71662616+L3M0NFLUX@users.noreply.github.com>
2021-11-15 19:15:25 -0800

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,11 +1,23 @@
-import selenium, keyboard, time, os, math, random, pathfinding, ctypes, sys
+import selenium, time
+import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 print("Snake Bot\nby Owen")
-driver = webdriver.Chrome('chromedriver.exe')
+def w():
+  pyautogui.press('w')
+def a():
+  pyautogui.press('a')
+def s():
+  pyautogui.press('s')
+def d():
+  pyautogui.press('d')
+def press(key):
+  pyautogui.press(key)
+driver = webdriver.Chrome('./chromedriver')
 driver.set_window_size(626, 740)
 driver.get("https://www.google.com/fbx?fbx=snake_arcade")
-game = driver.find_elements_by_class_name("ahZmw")[0]
+game = driver.find_elements_by_class_name("cer0Bd")[0]
 gridsize = "17 x 15"
-keyboard.press_and_release('RETURN')
-keyboard.press_and_release('w')
+pyautogui.press('RETURN')
+time.sleep(0.5)
+press('s')
